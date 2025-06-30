679ea679fb88a824e32685d3345ad30ad666793b
Guilherme Beidaki Peres <guibperes@proton.me>
2025-06-30 13:17:06 -0300

    moved infraestructure to hugo

--- src/data/document.type.ts
+++
@@ -1,8 +0,0 @@
-export type Document = {
-  id: number;
-  listId: string;
-  type: "FINAL" | "ESTUDO";
-  name: string;
-  description: string;
-  link: string;
-};
--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import content from "@originjs/vite-plugin-content";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), content()],
-  base: "/cnbb-docs/",
-});
